a72ba7448715b433c1ccbf78e034daa9ea76d40f
ZAHNG-ship-IT <2186262581@qq.com>
2025-07-06 22:49:37 +0800

    Add files via upload
    10天应该可以结束比特的c语言吧

---
+++ day_4/_1.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+//分支语句  选择结果  顺序结构  循环结构
+//&&与符号  ||或符号
+int main()
+{
+    // printf("hello");
+    // return 0;
+//     int a = 1;
+// //     int b = 2;
+// //     // if(a == 1)
+// //     //     if(b == 1)
+// //     //         printf("%d",3);
+// //     // else//else只与最近匹配的if，代码也是不规范的
+// //     //     printf("%d",4);
+// //     if(a == 1)
+// //     {    if(b == 1)
+// //             printf("%d",3);
+// //             }
+// //     else
+// //         printf("%d",4);
+// // //规范代码
+// // //a == 5这样写没有问题，但是可以这样写 5 == a 这样写可以减少出错
+// //     return 0;
+//     for(int i = 1; i <= 100; i++)
+//     {
+//         if(0 != a %2)
+//         {   printf("%d\n",a);
+//             // a++;
+//         }
+//         a++;
+//         // else
+//             // printf("");
+//     }
+//     return 0;
+    // int a = 0;
+    // switch(a)
+    // {
+    //     case 1: 
+    //         printf("hello");//严格需要加break，因为只是入口  case 只能是常量，整形
+    //         //好好感受一下入口这个概念
+    //     default:
+    //     //不和所有case匹配的时候用default//编程好习惯就是加一个default
+    // }
+    // int i = 0;
+    // while(++i<=10)
+    // {
+    //     if(5 == i)  continue;//跳过5
+    //     printf("%d\n",i);
+
+    // }
+    // return 0;
+    // //break,退出所有的循环   ， continue  跳过本次
+    // int ch = getchar();//获取一个字符
+    // putchar(ch); //输出一个字符
+    // printf("\n%c",ch);
+    // //scanf不可以读空格，getchar可以读
+    printf("%c",87);
+
+}
